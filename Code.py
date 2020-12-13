989602f538d0c8cfb56b307d26f7d4b90e6357fc
Kayes <74409205+its-kayes@users.noreply.github.com>
2020-12-14 01:03:11 +0600

    Last Update

--- Code.py
+++ Code.py
@@ -3,20 +3,22 @@ from time import sleep
 pygame.init()
 window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
+                                      #Add Music
 pygame.mixer.init()
-pygame.mixer.music.load('Powfu - dead eyes ft.Ouse.mp3')
-pygame.mixer.music.play(start=15)
-#pygame.mixer.music.play()
-sleep(15)
-
-pygame.mixer.music.stop()
-sleep(5)
-
-
-pygame.mixer.music.load('Rema - Fame (A COLORS ENCORE ).mp3')
+pygame.mixer.music.load('Best Hacker music Deface Music EVER love khan.mp3')
 pygame.mixer.music.play()
+sleep(15)                             #Playing Music
 
-photo = pygame.image.load('1_vVsajU3uc1r3IPnXdIiCZA.jpg')
+pygame.mixer.music.stop()             #Stop Music
+sleep(1)
+
+                                      #Add New Music
+pygame.mixer.music.load('Hacker.mp3')
+pygame.mixer.music.play()
+                                      #Add Picture
+photo = pygame.image.load('Watch-Dogs-2-Widescreen-.jpg')
+
+                                      #Update Screen
 window.blit(photo, (0, 0))
 pygame.display.update()
-sleep(40)
+sleep(25)
